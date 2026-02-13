d9ddb921d43b8c6f45e6d64a7f92e2d364bad0bb
Alex Hayman <alexhayman@Tiens-MacBook-Pro.local>
2026-02-13 10:52:10 -0600

    added profiles app, model, admin sections

---
+++ interview/profiles/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from interview.profiles.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(BaseUserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active", "date_joined")
+    list_filter = ("is_staff", "is_superuser", "is_admin", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "username", "avatar")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_admin", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "first_name", "last_name"),
+        }),
+    )
