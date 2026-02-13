7caae99e9cfbd1c53baf3a555fdc4d8e96e53a5b
Alex Hayman <alexhayman@Tiens-MacBook-Pro.local>
2026-02-13 10:50:45 -0600

    admins sections for order & inventory

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from interview.inventory.models import Inventory, InventoryLanguage, InventoryTag, InventoryType
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "language", "created_at", "updated_at")
+    list_filter = ("type", "language", "tags")
+    search_fields = ("name",)
+    filter_horizontal = ("tags",)
+    ordering = ("-created_at",)
+
+
+@admin.register(InventoryLanguage)
+class InventoryLanguageAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(InventoryTag)
+class InventoryTagAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active", "created_at")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(InventoryType)
+class InventoryTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    search_fields = ("name",)
+    ordering = ("name",)
